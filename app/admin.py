6c056350930dcfc1a5e96e5e1bc079d54d52cf6d
pK <86195686+prudhvikollanapK@users.noreply.github.com>
2021-09-11 08:35:31 +0530

    Add files via upload
    App
    OTT
    Static
    Templates
    db.sqlite3
    manage

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Amazon,Netflix,Hotstar,Zee5,Aha,Mxplayer
+# Register your models here.
+admin.site.register(Amazon)
+admin.site.register(Netflix)
+admin.site.register(Hotstar)
+admin.site.register(Zee5)
+admin.site.register(Aha)
+admin.site.register(Mxplayer)
